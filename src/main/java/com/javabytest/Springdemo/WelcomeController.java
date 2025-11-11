222bb0e4c87be304289611649c2248d44c32af69
mlk <123862072+nikshitha293@users.noreply.github.com>
2025-11-11 10:50:58 +0700

    Update WelcomeController.java

--- src/main/java/com/javabytest/Springdemo/WelcomeController.java
+++ src/main/java/com/javabytest/Springdemo/WelcomeController.java
@@ -10,11 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/api")
 //@CrossOrigin(origins = "http://127.0.0.1:5500")
 public class WelcomeController {
 
-	  @GetMapping("/message")
+	  @GetMapping("/")
 	    public String getMessage() {
 	        return "Hello World From qwerty";
 	    }
